e7b1e91a2fab0710bfe899c9ebdd6ad722badf92
Vadim <44135514+vodemn@users.noreply.github.com>
2020-08-18 13:24:47 +0300

    Clean up

--- android/src/main/java/com/vodemn/system_features/SystemFeaturesPlugin.java
+++ android/src/main/java/com/vodemn/system_features/SystemFeaturesPlugin.java
@@ -30,35 +30,14 @@ public class SystemFeaturesPlugin implements MethodCallHandler {
 
     PackageManager pm = context.getPackageManager();
     switch (call.method) {
-    case "touchscreen":
-      result.success(pm.hasSystemFeature(PackageManager.FEATURE_TOUCHSCREEN));
-      break;
     case "camera":
       result.success(pm.hasSystemFeature("android.hardware.camera"));
       break;
     case "camera2":
       result.success(pm.hasSystemFeature("android.hardware.camera2.capability.manual_sensor"));
       break;
-    case "phone":
-      result.success(pm.hasSystemFeature("android.hardware.telephony"));
-      break;
-    case "faketouch":
-      result.success(pm.hasSystemFeature("android.hardware.faketouch"));
-      break;
-    case "nfc":
-      result.success(pm.hasSystemFeature("android.hardware.nfc"));
-      break;
-    case "microphone":
-      result.success(pm.hasSystemFeature("android.hardware.microphone"));
-      break;
     case "sensor":
       result.success(pm.hasSystemFeature("android.hardware.sensor.light"));
-      break;
-    case "leanback":
-      result.success(pm.hasSystemFeature("android.software.leanback"));
-      break;
-    case "isGoogleTV":
-      result.success(pm.hasSystemFeature("com.google.android.tv"));
       break;
 
     default:
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:system_features/system_features.dart';
 
 void main() => runApp(MyApp());
@@ -31,21 +30,10 @@ class _MyAppState extends State<MyApp> {
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
-    touchscreen = await SystemFeatures.touchscreen;
-    faketouch = await SystemFeatures.faketouch;
-    camera = await SystemFeatures.camera;
-    nfc = await SystemFeatures.nfc;
-    microphone = await SystemFeatures.isGoogleTv;
-    phone = await SystemFeatures.isGoogleTv;
     camera = await SystemFeatures.camera;
     camera2 = await SystemFeatures.camera2;
     sensor = await SystemFeatures.sensor;
-    leanback = await SystemFeatures.leanback;
-    isGoogleTv = await SystemFeatures.isGoogleTv;
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
     if (!mounted) return;
 
     setState(() {});
@@ -64,13 +52,6 @@ class _MyAppState extends State<MyApp> {
               Text('Camera: $camera\n'),
               Text('Camera2: $camera2\n'),
               Text('sensor: $sensor\n'),
-              Text('touchscreen: $touchscreen\n'),
-              Text('faketouch: $faketouch\n'),
-              Text('nfc: $nfc\n'),
-              Text('phone: $phone\n'),
-              Text('microphone: $microphone\n'),
-              Text('leanback: $leanback\n'),
-              Text('isGoogleTv: $isGoogleTv\n'),
             ],
           ),
         ),
